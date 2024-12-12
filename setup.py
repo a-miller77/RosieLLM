ab1adc4af4422cfb9b50e8db1a9c08b40de21f17
Aiden Miller <100258120+a-miller77@users.noreply.github.com>
2024-12-12 15:06:10 -0600

    updated dist

--- setup.py
+++ setup.py
@@ -1,25 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='rosiellm',
-    version='0.1.0',
-    author='Aiden Miller',
-    description='A project for RosieLLM',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/a-miller77/RosieLLM',
-    packages=find_packages(),
-    install_requires=[
-        'openai==0.10.2',
-        'paramiko==2.7.2',
-        'cryptography==3.4.7',
-        'python-dotenv==0.19.2',
-        'requests==2.26.0',
-    ],
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: Apache License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires=">=3.9",
-)
+setup()

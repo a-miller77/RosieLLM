4e1011f1c3ac18c296ab059208ada8874b37364e
Aiden Miller <100258120+a-miller77@users.noreply.github.com>
2024-12-12 14:42:04 -0600

    dist packages

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
     name='rosiellm',
     version='0.1.0',
     author='Aiden Miller',
-    author_email='milleraa@msoe.edu',
     description='A project for RosieLLM',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -15,6 +14,7 @@ setup(
         'paramiko==2.7.2',
         'cryptography==3.4.7',
         'python-dotenv==0.19.2',
+        'requests==2.26.0',
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
